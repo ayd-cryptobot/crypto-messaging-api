7da24c9f3dd8fb8204c80a6f87726e4fc13a35c3
Juan Moncada <juan.moncada@mercadolibre.com.co>
2022-03-22 21:04:38 -0500

    initial scaffolding

---
+++ src/main/java/com/ufps/cryptobot/CryptoBotApplication.java
@@ -0,0 +1,13 @@
+package com.ufps.cryptobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoBotApplication.class, args);
+    }
+
+}
