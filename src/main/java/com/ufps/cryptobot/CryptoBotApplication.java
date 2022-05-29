b3b7b4adc8913bba9348dcf4ec02c37192683241
Juan Moncada <juan.moncada@mercadolibre.com.co>
2022-05-29 17:53:23 -0500

    creating war file

--- src/main/java/com/ufps/cryptobot/CryptoBotApplication.java
+++ src/main/java/com/ufps/cryptobot/CryptoBotApplication.java
@@ -2,12 +2,19 @@ package com.ufps.cryptobot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CryptoBotApplication {
+public class CryptoBotApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CryptoBotApplication.class, args);
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(CryptoBotApplication.class);
     }
 
 }
